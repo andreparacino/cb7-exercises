6a4e4f521bc94e5871e94d0be6d3e99e9fa75d44
Andrea Paracino <82333243+andreparacino@users.noreply.github.com>
2023-05-04 13:03:18 +0200

    Update es1.js

--- 04-05-2023/es1.js
+++ 04-05-2023/es1.js
@@ -1,10 +1,5 @@
 function reverseArray(arr) {
-  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-    const temp = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
-  }
-  return arr;
+  return arr.reduce((reversed, element) => [element, ...reversed], []);
 }
 
 const testArray = [1, 2, 3, 4, 5];
